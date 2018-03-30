6ecece57be37afbaf05950fee51f8a0cac7edcff
Park-Ryeong-Min <somuch0321@gmail.com>
2018-03-30 15:17:34 +0900

    ping pong

--- src/socket.event/ping.js
+++ src/socket.event/ping.js
@@ -1,5 +1,6 @@
 module.exports = function (io, socket) {
     socket.on('ping', function (data) {
+        console.log('[FromCLIENT] ping - pong');
         io.to(socket.id).emit('pong', {
             result : 10,
             message : 'ping-pong with server and client'
