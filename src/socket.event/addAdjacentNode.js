cd440f8809ad31198d45628e7b2b71dd73e2a38f
Park-Ryeong-Min <somuch0321@gmail.com>
2018-04-09 01:42:56 +0900

    add function getDistanceFromLatLngInKm

--- src/socket.event/addAdjacentNode.js
+++ src/socket.event/addAdjacentNode.js
@@ -16,6 +16,20 @@ module.exports = function (io, socket) {
         var curNodeLng;
         var distance;
 
+        function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+            var R = 6371; // Radius of the earth in km
+            var dLat = deg2rad(lat2-lat1);  // deg2rad below
+            var dLon = deg2rad(lon2-lon1);
+            var a =
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+
         function onError(err) {
             console.error(err);
         }
@@ -41,7 +55,8 @@ module.exports = function (io, socket) {
                 latitude : curNodeLat,
                 longitude : curNodeLng
             }
-            distance = geo.getDistanceSimple(addNode, curNode);
+            //distance = geo.getDistanceSimple(addNode, curNode);
+            distance = getDistanceFromLatLonInKm(addNodeLat, addNodeLng, curNodeLat, curNodeLng);
             console.log('[GET_DISTANCE] distance : ' + distance);
             return new Promise(function (resolve, reject) {
                 resolve();
