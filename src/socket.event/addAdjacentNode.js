ac05aa8caa2413338f12429e68dcb819b09994ed
Park-Ryeong-Min <somuch0321@gmail.com>
2018-04-09 01:44:15 +0900

    fix 2

--- src/socket.event/addAdjacentNode.js
+++ src/socket.event/addAdjacentNode.js
@@ -16,6 +16,10 @@ module.exports = function (io, socket) {
         var curNodeLng;
         var distance;
 
+        function deg2rad(deg) {
+            return deg * (Math.PI/180)
+        }
+
         function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
             var R = 6371; // Radius of the earth in km
             var dLat = deg2rad(lat2-lat1);  // deg2rad below
